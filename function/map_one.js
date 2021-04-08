078b430810685a3f2491c8752fc220f91cccb415
wendleypf <wendleypf@gmail.com>
2021-04-07 21:30:56 -0300

    feat: example map one

---
+++ function/map_one.js
@@ -0,0 +1,37 @@
+/**
+ * @author wendleypf <wendleypf@gmail.com>
+ */
+
+const { mockProducts } = require('../mock/mock-product')
+const numbers = [1, 2, 3, 4, 5]
+/**
+ * @param {number} number
+ * @returns {number}
+ */
+const double = number => number * 2
+console.log(numbers.map(double))
+
+const names = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
+/**
+ * @param {string} text
+ * @returns {string}
+ */
+const firstLetter = text => text[0]
+console.log(names.map(firstLetter))
+
+const cart = mockProducts()
+
+/**
+ * @param {{amount: number, price: number, name: string}} product
+ * @returns {string}
+ */
+const productName = product => product.name
+/**
+ *
+ * @param  {{amount: number, price: number, name: string}} product
+ * @returns {number}
+ */
+const stockValueOfEachProduct = product => product.amount * product.price
+
+console.log(cart.map(productName))
+console.log(cart.map(stockValueOfEachProduct))
